aea73a8667b219095cd3d2dc0685f7ada42c5f90
mayank1-0 <mayank.mehmi@gmail.com>
2025-01-28 17:53:33 +0530

    Rohenii_final

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ require("dotenv").config();
 DBConnection()
 
 
-app.listen(process.env.PORT,()=> console.log(`server running on Port ${process.env.PORT}`))
+app.listen(process.env.PORT || 3000,()=> console.log(`server running on Port ${process.env.PORT} or 3000`))
