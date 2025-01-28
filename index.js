8796b724e957ba85635e4086599912211bad6073
mayank1-0 <mayank.mehmi@gmail.com>
2025-01-28 22:15:19 +0530

    vercel-views-bug

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const app = express();
 require("dotenv").config();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')));
